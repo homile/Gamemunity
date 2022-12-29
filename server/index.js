ad4261528592260e32b90f480e3377dc900ef939
homile <ssanho2@naver.com>
2022-12-29 16:15:32 +0900

    feat: 서버의 현재 경로와 상대 경로를 합침 path 라이브러리 사용

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+// 현재 경로와 상대 경로를 합쳐주는 라이브러리
+const path = require("path");
 const app = express();
 const port = 5001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// static으로 활용할 폴더 설정
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // server 실행
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});

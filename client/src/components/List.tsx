b7ab0ba1fcc218fb89a58713a552f7b8d87ff9bb
homile <ssanho2@naver.com>
2022-12-30 01:30:26 +0900

    feat: 서버통신 구현

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Props } from "../App";
 
 const List = (props: Props) => {
   const { contentList } = props;
 
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    let body = { text: "Hello" };
+
+    axios
+      .post("/api/test", body)
+      .then((res) => {
+        console.log(res);
+        setText(res.data.text);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
+      <h3>List</h3>
+      <h3>{text}</h3>
       {contentList.map((content, idx) => {
         return (
           <div key={idx} style={{ width: "100%", marginLeft: "1rem" }}>

59b4278b7269a0e6f6beb34c4aa746237beddc3a
homile <ssanho2@naver.com>
2022-12-30 18:21:43 +0900

    rename: API 폴더분리 Route 설정

--- server/index.js
+++ server/index.js
@@ -13,8 +13,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const { Post } = require("./Model/Post.js");
-const { Counter } = require("./Model/Counter.js");
+app.use("/api/post", require("./Router/post.js"));
 
 // server 실행
 app.listen(port, () => {
@@ -36,75 +35,4 @@ app.get("/", (req, res) => {
 // 모든 url에서 반응
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-// 게시글 작성
-app.post("/api/post/submit", (req, res) => {
-  let temp = req.body;
-  Counter.findOne({ name: "counter" })
-    .exec()
-    .then((counter) => {
-      temp.postNum = counter.postNum;
-      const CommunityPost = new Post(temp);
-      CommunityPost.save().then(() => {
-        Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } }).then(() => {
-          res.status(200).json({ success: true });
-        });
-      });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-// 게시글 리스트 보기
-app.post("/api/post/list", (req, res) => {
-  Post.find()
-    .exec()
-    .then((doc) => {
-      res.status(200).json({ success: true, postList: doc });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-// 게시글 상세보기
-app.post("/api/post/detail", (req, res) => {
-  Post.findOne({ postNum: Number(req.body.postNum) })
-    .exec()
-    .then((doc) => {
-      res.status(200).json({ success: true, post: doc });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-// 게시글 수정
-app.post("/api/post/edit", (req, res) => {
-  let temp = {
-    title: req.body.title,
-    content: req.body.content,
-  };
-  Post.updateOne({ postNum: Number(req.body.postNum) }, { $set: temp })
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
-});
-
-// 게시글 삭제
-app.post("/api/post/delete", (req, res) => {
-  Post.deleteOne({ postNum: Number(req.body.postNum) })
-    .exec()
-    .then(() => {
-      res.status(200).json({ success: true });
-    })
-    .catch((err) => {
-      res.status(400).json({ success: false });
-    });
 });

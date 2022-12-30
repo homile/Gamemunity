94cb5b10dec611713fd3da377d6f04924827268c
homile <ssanho2@naver.com>
2022-12-30 18:16:43 +0900

    feat:게시글 삭제 기능 구현

--- client/src/components/post/Detail.tsx
+++ client/src/components/post/Detail.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { BtnDiv, Post, PostDiv } from "../../style/PostDetailCSS";
 
 interface PostInfoType {
@@ -12,6 +12,7 @@ interface PostInfoType {
 
 const Detail = () => {
   let params = useParams();
+  const navigate = useNavigate();
 
   const [postInfo, setPostInfo] = useState<PostInfoType>({ id: "", title: "", content: "", postNum: null });
 
@@ -32,6 +33,26 @@ const Detail = () => {
       });
   }, []);
 
+  const DeleteHandler = () => {
+    if (window.confirm("정말로 삭제하시겠습니까?")) {
+      let body = {
+        postNum: params.postNum,
+      };
+
+      axios
+        .post("/api/post/delete", body)
+        .then((res) => {
+          if (res.data.success) {
+            alert("게시글이 삭제되었습니다.");
+            navigate("/");
+          }
+        })
+        .catch((err) => {
+          alert("게시글 삭제에 실패하였습니다.");
+        });
+    }
+  };
+
   return (
     <PostDiv>
       <Post>
@@ -42,9 +63,9 @@ const Detail = () => {
         <Link to={`/edit/${postInfo.postNum}`}>
           <button className="edit">수정</button>
         </Link>
-        <Link to="">
-          <button className="delete">삭제</button>
-        </Link>
+        <button className="delete" onClick={() => DeleteHandler()}>
+          삭제
+        </button>
       </BtnDiv>
     </PostDiv>
   );
--- server/index.js
+++ server/index.js
@@ -96,3 +96,15 @@ app.post("/api/post/edit", (req, res) => {
       res.status(400).json({ success: false });
     });
 });
+
+// 게시글 삭제
+app.post("/api/post/delete", (req, res) => {
+  Post.deleteOne({ postNum: Number(req.body.postNum) })
+    .exec()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false });
+    });
+});

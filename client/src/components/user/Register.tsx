061152f9b8094072c63122a0ae1b72500e9f2dea
homile <ssanho2@naver.com>
2023-01-19 23:57:42 +0900

    feat: firebase 회원가입 기능 구현

--- client/src/components/user/Register.tsx
+++ client/src/components/user/Register.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import LoginDiv from "../../style/UserCSS";
+import firebase from "../../firebase";
 
 const Register = () => {
   const [nickName, setNickName] = useState("");
@@ -7,12 +8,29 @@ const Register = () => {
   const [PW, setPW] = useState("");
   const [PWConfirm, setPWComfirm] = useState("");
 
+  const RegistrtFunc = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (!(nickName && email && PW && PWConfirm)) {
+      return alert("모든 값을 채워주세요!");
+    }
+    if (PW !== PWConfirm) {
+      return alert("비밀번호와 비밀번호 확인 값은 같아야 합니다.");
+    }
+    let createdUser = await firebase.auth().createUserWithEmailAndPassword(email, PW);
+
+    await createdUser.user?.updateProfile({
+      displayName: nickName,
+    });
+
+    console.log(createdUser.user);
+  };
+
   return (
     <LoginDiv>
       <form>
         <label htmlFor="nick-name">닉네임</label>
         <input id="nick-name" type="name" value={nickName} onChange={(e) => setNickName(e.currentTarget.value)} />
-        <label htmlFor="email">닉네임</label>
+        <label htmlFor="email">이메일</label>
         <input id="email" type="email" value={email} onChange={(e) => setEmail(e.currentTarget.value)} />
         <label htmlFor="password">비밀번호</label>
         <input id="password" type="password" value={PW} onChange={(e) => setPW(e.currentTarget.value)} />
@@ -23,7 +41,7 @@ const Register = () => {
           value={PWConfirm}
           onChange={(e) => setPWComfirm(e.currentTarget.value)}
         />
-        <button>회원가입</button>
+        <button onClick={(e) => RegistrtFunc(e)}>회원가입</button>
       </form>
     </LoginDiv>
   );

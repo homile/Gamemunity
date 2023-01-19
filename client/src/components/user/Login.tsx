16b5aef6a3e95b60c81ca7e12442e3cb223ddbd9
homile <ssanho2@naver.com>
2023-01-19 23:38:15 +0900

    feat: 로그인, 회원가입 컴포넌트 생성

--- client/src/components/user/Login.tsx
+++ client/src/components/user/Login.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import LoginDiv from "../../style/UserCSS";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [PW, setPW] = useState("");
 
   return (
     <LoginDiv>
@@ -11,9 +13,16 @@ const Login = () => {
         <label htmlFor="email">이메일</label>
         <input id="email" type="email" value={email} onChange={(e) => setEmail(e.currentTarget.value)} />
         <label htmlFor="password">비밀번호</label>
-        <input id="password" type="password" value={password} onChange={(e) => setPassword(e.currentTarget.value)} />
+        <input id="password" type="password" value={PW} onChange={(e) => setPW(e.currentTarget.value)} />
         <button>로그인</button>
-        <button>회원가입</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/register");
+          }}
+        >
+          회원가입
+        </button>
       </form>
     </LoginDiv>
   );
--- client/src/components/user/Register.tsx
+++ client/src/components/user/Register.tsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import LoginDiv from "../../style/UserCSS";
 
 const Register = () => {
+  const [nickName, setNickName] = useState("");
+  const [email, setEmail] = useState("");
+  const [PW, setPW] = useState("");
+  const [PWConfirm, setPWComfirm] = useState("");
+
   return (
     <LoginDiv>
       <form>
         <label htmlFor="nick-name">닉네임</label>
-        <input id="nick-name" type="name" />
+        <input id="nick-name" type="name" value={nickName} onChange={(e) => setNickName(e.currentTarget.value)} />
         <label htmlFor="email">닉네임</label>
-        <input id="email" type="email" />
+        <input id="email" type="email" value={email} onChange={(e) => setEmail(e.currentTarget.value)} />
         <label htmlFor="password">비밀번호</label>
-        <input type="password" />
+        <input id="password" type="password" value={PW} onChange={(e) => setPW(e.currentTarget.value)} />
         <label htmlFor="password-check">비밀번호확인</label>
-        <input id="password-check" type="password" />
+        <input
+          id="password-check"
+          type="password"
+          value={PWConfirm}
+          onChange={(e) => setPWComfirm(e.currentTarget.value)}
+        />
         <button>회원가입</button>
       </form>
     </LoginDiv>

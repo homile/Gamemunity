233ff6aa86c2e9b03535528ba06277218b3ac7a5
homile <ssanho2@naver.com>
2023-01-26 22:08:25 +0900

    feat: 댓글 업로드 기능 구현

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/post", require("./Router/post.js"));
 app.use("/api/user", require("./Router/user.js"));
+app.use("/api/reple", require("./Router/reple.js"));
 
 // server 실행
 app.listen(port, () => {

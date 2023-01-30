94e28672de404d6cda9d4b2a303429bccec9c397
homile <ssanho2@naver.com>
2023-01-30 21:18:41 +0900

    feat: 프로필 이미지 수정 기능 구현

--- client/src/components/user/MyPage.tsx
+++ client/src/components/user/MyPage.tsx
@@ -1,20 +1,85 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Avatar from "react-avatar";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../../Reducer/store";
+import firebase from "../../firebase";
 
 const MyPage = () => {
   const user = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
 
+  const [currentImage, setCurrentImage] = useState("");
+
+  const ImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let formData: any = new FormData();
+    formData.append("file", e.target.files?.[0]);
+
+    axios.post("/api/user/profile/img", formData).then((res) => {
+      setCurrentImage(res.data.filePath);
+    });
+  };
+
+  const saveProfile = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    try {
+      await firebase?.auth()?.currentUser?.updateProfile({
+        photoURL: currentImage,
+      });
+    } catch (error) {
+      return alert("프로필 저장에 실패하였습니다.");
+    }
+
+    let body = {
+      photoURL: currentImage,
+      uid: user.uid,
+    };
+
+    axios.post("/api/user/profile/update", body).then((res) => {
+      if (res.data.success) {
+        alert("프로필 저장에 성공하였습니다.");
+        window.location.reload();
+      } else {
+        return alert("프로필 저장에 실패하였습니다.");
+      }
+    });
+  };
+
   useEffect(() => {
-    console.log(user);
     if (!user.isLoading && !user.accessToken) {
       navigate("/login");
+    } else {
+      setCurrentImage(user.photoURL);
     }
   }, [user]);
 
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <form
+        style={{
+          width: "50%",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "2rem",
+        }}
+      >
+        <label>
+          <input type="file" accept="image/*" style={{ display: "none" }} onChange={(e) => ImageUpload(e)} />
+          <Avatar
+            size="100"
+            round={true}
+            src={currentImage}
+            style={{ border: "1px solid #c6c6c6", cursor: "pointer" }}
+          />
+        </label>
+        <button onClick={(e) => saveProfile(e)}>저장</button>
+      </form>
+    </div>
+  );
 };
 
 export default MyPage;
--- server/Router/user.js
+++ server/Router/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const { User } = require("../Model/User.js");
 const { Counter } = require("../Model/Counter.js");
+const setUpload = require("../util/upload.js");
 
 router.post("/register", (req, res) => {
   let temp = req.body;
@@ -34,6 +35,24 @@ router.post("/namecheck", (req, res) => {
     })
     .catch((err) => {
       console.log(err);
+      res.status(400).json({ success: false });
+    });
+});
+
+router.post("/profile/img", setUpload("gamemunity/user"), (req, res, next) => {
+  res.status(200).json({ success: true, filePath: res.req.file.location });
+});
+
+router.post("/profile/update", (req, res) => {
+  let temp = {
+    photoURL: req.body.photoURL,
+  };
+  User.updateOne({ uid: req.body.uid }, { $set: temp })
+    .exec()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((err) => {
       res.status(400).json({ success: false });
     });
 });

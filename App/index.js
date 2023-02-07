4f0aad1a8a6f4ad2df9689054f6f88337ee8021b
homile <ssanho2@naver.com>
2023-02-07 18:42:43 +0900

    chore: 배포를 위한 파일 구성 변경

--- App/index.js
+++ App/index.js
@@ -4,19 +4,19 @@ const path = require("path");
 // mongodb 사용하기 쉽도록 해주는 라이브러리
 const mongoose = require("mongoose");
 const app = express();
-const port = 5001;
-const config = require("./config/key.js");
+const port = process.env.PORT || 5001;
+const config = require("./server/config/key.js");
 
 // static으로 활용할 폴더 설정
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 // 이미지 사용할 수 있게 설정
 app.use("/image", express.static("./image"));
 // body에 담긴 내용을 볼 수 있게 해줌
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/post", require("./Router/post.js"));
-app.use("/api/user", require("./Router/user.js"));
-app.use("/api/reple", require("./Router/reple.js"));
+app.use("/api/post", require("./server/Router/post.js"));
+app.use("/api/user", require("./server/Router/user.js"));
+app.use("/api/reple", require("./server/Router/reple.js"));
 
 // server 실행
 app.listen(port, () => {
@@ -32,10 +32,10 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // 모든 url에서 반응
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
